4626967f6a5e3cf84000791974aac21903623849
kakao-daniel-hoon <daniel.hoon@kakaocorp.com>
2026-03-03 10:59:45 +0900

    test(order): 에러 케이스 인수테스트 추가
    - createOrder_InsufficientStock: 재고 초과 주문 시 400

--- src/test/java/gift/order/OrderAcceptanceTest.java
+++ src/test/java/gift/order/OrderAcceptanceTest.java
@@ -87,4 +87,18 @@ class OrderAcceptanceTest {
                 .content(objectMapper.writeValueAsString(request)))
             .andExpect(status().isNotFound());
     }
+
+    @Test
+    @DisplayName("재고보다 많은 수량으로 주문하면 400을 반환한다")
+    void createOrder_InsufficientStock() throws Exception {
+        String token = obtainAccessToken();
+        // optionId=1 (맥북 스페이스 블랙 / M1 Pro, 재고 10개)
+        var request = new OrderRequest(1L, 9999, null);
+
+        mockMvc.perform(post("/api/orders")
+                .header("Authorization", "Bearer " + token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isBadRequest());
+    }
 }
