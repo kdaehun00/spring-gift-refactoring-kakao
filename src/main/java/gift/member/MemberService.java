c3f3230ffc088d4f27d2a1cdebf425bb810f8749
kakao-daniel-hoon <daniel.hoon@kakaocorp.com>
2026-03-04 13:27:46 +0900

    refactor: @Transactional 추가로 인한 save 메서드 제거

--- src/main/java/gift/member/MemberService.java
+++ src/main/java/gift/member/MemberService.java
@@ -52,7 +52,6 @@ public class MemberService {
         Member member = memberRepository.findById(id)
             .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
         member.update(email, password);
-        memberRepository.save(member);
     }
 
     @Transactional
